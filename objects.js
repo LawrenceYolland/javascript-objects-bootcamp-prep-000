3e5f579a0e420527cb34efd730d1e3719a8c15a6
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-04-25 10:20:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,9 @@
 var playlist = {'Radical Face': 'Ghost Towns', 'Sufjan Stevens': 'Death with Dignity'};
+
+function updatePlaylist(obj, newKey, newVal) {
+  return Object.assign(obj, {newKey: newVal})
+}
+
+funtion removeFromPlaylist(obj, key) {
+  return obj.delete(key)
+}
